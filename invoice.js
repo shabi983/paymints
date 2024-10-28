1ef77f98980bc5240f16104b786d356f5f66a885
shabi983 <83406565+shabi983@users.noreply.github.com>
2024-10-28 20:31:51 +0500

    invoice.js

--- invoice.js
+++ invoice.js
@@ -166,23 +166,3 @@
     } else {
       document.getElementById("invoice983").style.display = "block";
     }
-
-
-  function checkDevice() {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    const desktopContent = document.querySelector('.fordesktop983');
-    const mobileContent = document.querySelector('.formobile983');
-
-    if (isMobile) {
-      // Hide desktop content and show mobile message
-      desktopContent.style.display = 'none';
-      mobileContent.style.display = 'block';
-    } else {
-      // Show desktop content and hide mobile message
-      desktopContent.style.display = 'block';
-      mobileContent.style.display = 'none';
-    }
-  }
-
-  // Run checkDevice when the DOM is fully loaded
-  document.addEventListener("DOMContentLoaded", checkDevice);
